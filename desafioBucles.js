66a55285d20b941a27c08d48d38a3ee38928d3a7
deysiSc76 <deysi.santacruz76@gmail.com>
2024-04-17 23:54:25 -0400

    ejericio de Bucles

---
+++ desafioBucles.js
@@ -0,0 +1,32 @@
+/** ejericio1 */
+for(let i = 1; i <= 20; i++) {
+    if(i%2 != 0) {
+        console.log(i);
+    }
+}
+/** ejericio2 */
+for(let i = 100; i >= 0; i--) {
+    if(i%3 == 0) {
+        console.log(i);
+    }
+}
+
+/** ejercicio3 */
+for(let i = 4; i>=-3.5; i-=1.5) {
+    console.log(i);
+}
+
+/** ejercicio4 */
+let x=0;
+for(let i= 1; i <= 100; i++) {
+    x+=i;
+ }
+ console.log(x);
+ /** ejercicio5 */
+ let a=1;
+for(let i= 1; i <= 12; i++) {
+    a*=i;
+    
+ }
+ console.log(a);
+ 
